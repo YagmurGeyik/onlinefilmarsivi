1866b0e9900340da530b7e6ece98e63a4ea818c2
YagmurGeyik <yagmur1912@outlook.com>
2025-07-22 17:28:06 +0300

    son düzenlemeler yapıldı.

--- onlinefilmarsivi/src/components/Navbar.jsx
+++ onlinefilmarsivi/src/components/Navbar.jsx
@@ -1,20 +1,33 @@
-import React, { useState } from 'react';
-import { Navbar, Container, Nav, Form, FormControl } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import { BsSun, BsMoon, BsSearch } from 'react-icons/bs';
+import React, { useState } from "react";
+import { Navbar, Container, Nav, Form, FormControl } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { BsSun, BsMoon, BsSearch } from "react-icons/bs";
 import "../styles/index.css";
 
-function NavigationBar({ searchTerm, setSearchTerm, toggleTheme, theme, onLoginClick, onRegisterClick }) {
+function NavigationBar({
+  searchTerm,
+  setSearchTerm,
+  toggleTheme,
+  theme,
+  onLoginClick,
+  onRegisterClick,
+}) {
   const [showSearch, setShowSearch] = useState(false);
 
   return (
     <Navbar className="my-navbar" expand="lg">
       <Container className="d-flex justify-content-between align-items-center">
-        <Navbar.Brand as={Link} to="/" className="me-3">CineLİST</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/" className="me-3">
+          CineLİST
+        </Navbar.Brand>
 
         <Nav className="d-none d-lg-flex me-auto">
-          <Nav.Link as={Link} to="/">Anasayfa</Nav.Link>
-          <Nav.Link as={Link} to="/favorites">Favoriler</Nav.Link>
+          <Nav.Link as={Link} to="/">
+            Anasayfa
+          </Nav.Link>
+          <Nav.Link as={Link} to="/favorites">
+            Favoriler
+          </Nav.Link>
         </Nav>
 
         {showSearch && (
@@ -30,26 +43,22 @@ function NavigationBar({ searchTerm, setSearchTerm, toggleTheme, theme, onLoginC
           </Form>
         )}
 
-        <div className="d-flex align-items-center gap-2">
+        <div className="nav-buttons d-flex align-items-center gap-2">
           <button
             className="btn btn-outline-light"
             onClick={() => setShowSearch(!showSearch)}
-            title="Ara"
           >
             <BsSearch />
           </button>
-
-          <button
-            className="btn btn-outline-light"
-            onClick={toggleTheme}
-            title="Tema Değiştir"
-          >
+          <button className="btn btn-outline-light" onClick={toggleTheme}>
             {theme === "light" ? <BsMoon size={20} /> : <BsSun size={20} />}
           </button>
-
-          {/* Giriş ve Kayıt bağlantıları butona çevrildi */}
-          <button className="btn btn-outline-light ms-2" onClick={onLoginClick}>Giriş Yap</button>
-          <button className="btn btn-light ms-2" onClick={onRegisterClick}>Kayıt Ol</button>
+          <button className="btn btn-outline-light" onClick={onLoginClick}>
+            Giriş Yap
+          </button>
+          <button className="btn btn-light" onClick={onRegisterClick}>
+            Kayıt Ol
+          </button>
         </div>
       </Container>
     </Navbar>
